c0c6d54a660457e65ba5f033707eca24aa0b9318
RAMANUJAM <pramanujam68@gmail.com>
2025-07-25 18:55:00 +0530

    Initial commit

---
+++ OneDrive/Desktop/RESPONSIVELANDING PAGE/script.js
@@ -0,0 +1,12 @@
+// Change navbar style on scroll
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+document.querySelector('.btn').addEventListener('click', () => {
+  alert("Thanks for clicking!");
+});
